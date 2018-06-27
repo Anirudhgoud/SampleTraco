f9225eee081e9e89a77276bb2c5ab23808036c6b
Vishal <vishal@cybrilla.com>
2018-06-27 18:03:14 +0530

    Removed unreferenced code

--- app/src/main/java/com/tracotech/services/room/AppDatabase.java
+++ app/src/main/java/com/tracotech/services/room/AppDatabase.java
@@ -6,26 +6,12 @@ import android.arch.persistence.room.DatabaseConfiguration;
 import android.arch.persistence.room.InvalidationTracker;
 import android.arch.persistence.room.RoomDatabase;
 
-import com.tracotech.tracoman.services.room.daos.DeliveryOrderDao;
-import com.tracotech.tracoman.services.room.daos.DeliveryOrderItemDao;
-import com.tracotech.tracoman.services.room.daos.DriverDao;
-import com.tracotech.tracoman.services.room.daos.ReturnOrderDao;
-import com.tracotech.tracoman.services.room.daos.StockProductDao;
-import com.tracotech.tracoman.services.room.daos.WarehouseDao;
-import com.tracotech.tracoman.services.room.entities.DeliveryOrderEntity;
-import com.tracotech.tracoman.services.room.entities.DriverEntity;
-import com.tracotech.tracoman.services.room.entities.OrderItemEntity;
-import com.tracotech.tracoman.services.room.entities.ProductEntity;
-import com.tracotech.tracoman.services.room.entities.ReturnOrderEntity;
-import com.tracotech.tracoman.services.room.entities.StockProductEntity;
-import com.tracotech.tracoman.services.room.entities.WarehouseEntity;
+
 
 /**
  * Created by vishalm on 09/02/18.
  */
-@Database(entities = {DeliveryOrderEntity.class, DriverEntity.class,
-        OrderItemEntity.class, ProductEntity.class, StockProductEntity.class, ReturnOrderEntity.class,
-        WarehouseEntity.class},
+@Database(entities = {},
         version = 1)
 //@TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase{
@@ -44,11 +30,5 @@ public abstract class AppDatabase extends RoomDatabase{
         return null;
     }
 
-    public abstract DeliveryOrderDao deliveryOrderDao();
-    public abstract DriverDao driverDao();
-    public abstract DeliveryOrderItemDao deliveryOrderItemDao();
 
-    public abstract StockProductDao stockProductDao();
-    public abstract ReturnOrderDao returnOrderDao();
-    public abstract WarehouseDao warehouseDao();
 }
