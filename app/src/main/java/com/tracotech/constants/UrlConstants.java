6ae8e963621bd56ec914f93d73e5ef6ca7227f3c
Anirudh <auroraanirudh@gmail.com>
2018-07-09 15:03:13 +0530

    base url update

--- app/src/main/java/com/tracotech/constants/UrlConstants.java
+++ app/src/main/java/com/tracotech/constants/UrlConstants.java
@@ -3,8 +3,10 @@ package com.tracotech.constants;
 
 public interface UrlConstants {
 
+    String BASE_URL = "http://dashboard.tracouat.cybrilla.io/v1";
+
     //Login
-    String LOGIN_URL = "http://dashboard.tracouat.cybrilla.io/v1" + "/login";
+    String LOGIN_URL = BASE_URL + "/login";
 
 
 }
